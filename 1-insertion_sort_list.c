8a09a9becfe9dcb0ae1fd3adb9779d0f76a122c7
salahbesbes <salah_besbes@ymail.com>
2021-02-03 18:06:06 +0100

    insert

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,7 +7,14 @@
  */
 void swap(listint_t *left, listint_t *right)
 {
-
+	if (left->prev != NULL)
+		left->prev->next = right;
+	if (right->next != NULL)
+		right->next->prev = left;
+	left->next = right->next;
+	right->prev = left->prev;
+	left->prev = right;
+	right->next = left;
 }
 
 /**
@@ -17,5 +24,23 @@ void swap(listint_t *left, listint_t *right)
  */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *i, *j;
 
+	if (!list || !(*list) || !((*list)->next))
+		return;
+	i = (*list)->next;
+	while (i)
+	{
+		j = i;
+		while (j->prev && (j->prev)->n > j->n)
+		{
+			if (i == j)
+				i = j->prev;
+			if (j->prev == *list)
+				*list = j;
+			swap(j->prev, j);
+			print_list(*list);
+		}
+		i = i->next;
+	}
 }
